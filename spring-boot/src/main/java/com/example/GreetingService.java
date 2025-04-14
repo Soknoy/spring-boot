971f2bb6e2e14f3be2fb22e703ca2b6bbb2b7282
Soknoy <gonhxh18@gmail.com>
2025-04-14 21:06:35 +0800

    update source code

---
+++ spring-boot/src/main/java/com/example/GreetingService.java
@@ -0,0 +1,7 @@
+public interface GreetingService {
+
+    // Use: 
+    // - Go to Implementations (Ctrl+F12) to see all classes implementing this interface
+    // - Find All Implementations to track actual method bodies in classes like GreetingServiceImpl
+    String getGreeting();
+}
